89fa580235a00dbd3d89a8c2ed3747510f19a268
papahughes <97761989+papahughes@users.noreply.github.com>
2022-01-21 16:31:06 -0500

    Add files via upload

---
+++ answers.md
@@ -0,0 +1,7 @@
+
+Answer 1: git version 2.17.1
+
+Answer 2: user.name=Papa Hughes
+	  user.email=ph096820@ohio.edu
+
+Answer 3: When you type git --help, a very large help directory shows up and gives a bunch of information. 
---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Papa Hughes
+ *        Date: 1\21\2022
+ * Description: Lab 02
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
